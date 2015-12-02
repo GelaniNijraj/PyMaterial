0220580b6734881cc2e78d7327b834934559954a
root <Nijraj Gelani>
2015-12-02 21:51:37 +0530

    Some changes

--- MComponents/MRadioButton.py
+++ MComponents/MRadioButton.py
@@ -3,15 +3,21 @@ __author__ = 'MaitreyaBuddha'
 from PySide.QtGui import *
 from PySide.QtCore import *
 from MUtilities.MShape import MShape
+from MUtilities import MColors
 from MAnimations.MFade import MFadeOut, MFadeIn
+from MAnimations.MScale import MScaleOut, MScaleIn
 
 
 class OuterRing(MShape):
     def __init__(self):
         MShape.__init__(self)
-        self.setWidth(17)
-        self.setHeight(17)
-        self.__color = QColor("#F00")
+        self.setMaxWidth(20)
+        self.setMaxHeight(20)
+        self.setWidth(20)
+        self.setHeight(20)
+        self.setMarginX(15)
+        self.setMarginY(15)
+        self.__color = MColors.PRIMARY_COLOR
 
     def paintEvent(self, event):
         painter = QPainter()
@@ -19,17 +25,21 @@ class OuterRing(MShape):
         painter.setRenderHint(QPainter.Antialiasing)
         painter.setOpacity(self.getOpacity())
         painter.setPen(QPen(self.__color, 2))
-        painter.drawEllipse(self.getX(), self.getX(), self.getWidth(), self.getHeight())
+        painter.drawEllipse(self.getX()+self.getMarginX(), self.getY()+self.getMarginY(), self.getWidth(), self.getHeight())
         painter.end()
 
 
 class InnerCircle(MShape):
     def __init__(self):
         MShape.__init__(self)
-        self.setWidth(9)
-        self.setHeight(9)
-        self.__pen = QPen(QColor(255, 255, 255, 0), 0)
-        self.__color = QColor("#F00")
+        self.setMaxWidth(10)
+        self.setMaxHeight(10)
+        self.setWidth(10)
+        self.setHeight(10)
+        self.setMarginX(20)
+        self.setMarginY(20)
+        self.__pen = QPen(MColors.PRIMARY_COLOR, 0)
+        self.__color = MColors.PRIMARY_COLOR
 
     def paintEvent(self, event):
         painter = QPainter()
@@ -38,30 +48,33 @@ class InnerCircle(MShape):
         painter.setOpacity(self.getOpacity())
         painter.setPen(self.__pen)
         painter.setBrush(self.__color)
-        painter.drawEllipse(self.getX(), self.getX(), self.getWidth(), self.getHeight())
+        painter.drawEllipse(self.getX()+self.getMarginX(), self.getY()+self.getMarginY(), self.getWidth(), self.getHeight())
         painter.end()
 
 
 class MRadioButton(MShape):
     def __init__(self):
         MShape.__init__(self)
+        self.setFixedSize(QSize(50, 50))
         self.innerCircle = InnerCircle()
-        self.innerCircle.setX(self.getX() + 5)
-        self.innerCircle.setY(self.getY() + 5)
         self.outerRing = OuterRing()
-        self.outerRing.setX(self.getX() + 1)
-        self.outerRing.setY(self.getY() + 1)
-        layout = QGridLayout()
-        layout.addWidget(self.innerCircle, 0, 0)
-        layout.addWidget(self.outerRing, 0, 0)
-        self.setLayout(layout)
+        self.addLayoutItem(self.outerRing, 0, 0)
+        self.addLayoutItem(self.innerCircle, 0, 0)
+        self.setLayout(self.getLayout())
         self.__checked = True
 
     def mousePressEvent(self, event):
         if self.__checked:
-            MFadeOut.start(self.innerCircle)
+            MScaleOut.start(self.innerCircle, 0.006)
             self.__checked = False
         else:
-            MFadeIn.start(self.innerCircle)
+            MScaleIn.start(self.innerCircle, 0.006)
             self.__checked = True
 
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        painter.setClipRect(QRect(0, 0, 50, 50))
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(QPen(QColor('#FFF'), 1))
+        painter.end()
--- MUtilities/MShape.py
+++ MUtilities/MShape.py
@@ -1,6 +1,7 @@
 __author__ = 'MaitreyaBuddha'
 
-from PySide.QtGui import QWidget
+from PySide.QtGui import QWidget, QGridLayout, QApplication, QGraphicsLayout
+from PySide.QtCore import QMargins
 
 
 class MShape(QWidget):
@@ -8,24 +9,43 @@ class MShape(QWidget):
         QWidget.__init__(self)
         self.__x = 0
         self.__y = 0
-        self.__width = None
-        self.__height = None
+        self.__width = 0
+        self.__height = 0
         self.__opacity = 1
         self.__clip = None
-        self.__maxWidth = None
-        self.__maxHeight = None
+        self.__maxWidth = 0
+        self.__maxHeight = 0
+        self.__minWidth = 0
+        self.__minHeight = 0
         self.__maxOpacity = 1
         self.__minOpacity = 0
+        self.__marginX = 0
+        self.__marginY = 0
+        # Animation related flafs
         self.__fadeAnimation = False
+        self.__scaleAnimation = False
+        # Defining the layout which will hold the child shapes of the widget
+        self.__layout = QGridLayout()
+        self.__layout.setVerticalSpacing(0)
+        self.__layout.setHorizontalSpacing(0)
+        self.__layout.setContentsMargins(QMargins(0, 0, 0, 0))
+        self.__children = []
+
+    def move(self, x, y):
+        QWidget.move(self, x, y)
+        self.__x = x
+        self.__y = y
+        self.update()
+        QApplication.processEvents()
 
     def setFadeAnimationRunning(self, val):
         self.__fadeAnimation = val
 
-    def setX(self, x):
-        self.__x = x
+    def setScaleAnimationRunning(self, val):
+        self.__scaleAnimation = val
 
     def setY(self, y):
-        self.__y = y
+        self.move(self.__x, y)
 
     def setOpacity(self, opacity):
         self.__opacity = opacity
@@ -38,24 +58,52 @@ class MShape(QWidget):
 
     def setHeight(self, height):
         self.__height = height
+        if self.__height < self.__minHeight:
+            self.__height = self.__minHeight
+        elif self.__height > self.__maxHeight:
+            self.__height= self.__maxHeight
 
     def setWidth(self, width):
         self.__width = width
+        if self.__width < self.__minWidth:
+            self.__width = self.__minWidth
+        elif self.__width > self.__maxWidth:
+            self.__width = self.__maxWidth
 
     def setClipRegion(self, clip):
         self.__clip = clip
 
     def setMaxWidth(self, width):
+        """
+        Must be called before setWidth()
+        :param width: int
+        :return:
+        """
         self.__maxWidth = width
 
     def setMaxHeight(self, height):
+        """
+        Must be called before setHeight()
+        :param width: int
+        :return:
+        """
         self.__maxHeight = height
 
+    def setMarginX(self, x):
+        self.__marginX = x
+
+    def setMarginY(self, y):
+        self.__marginY = y
+
+    def addLayoutItem(self, item, x, y):
+        self.__children.append(item)
+        self.__layout.addWidget(item, x, y)
+
     def getX(self):
-        return self.__x
+        return self.x()
 
     def getY(self):
-        return self.__y
+        return self.y()
 
     def getOpacity(self):
         return self.__opacity
@@ -75,11 +123,29 @@ class MShape(QWidget):
     def getMaxHeight(self):
         return self.__maxHeight
 
+    def getMinWidth(self):
+        return self.__minWidth
+
+    def getMinHeight(self):
+        return self.__minHeight
+
     def getMinOpacity(self):
         return self.__minOpacity
 
     def getMaxOpacity(self):
         return self.__maxOpacity
 
+    def getMarginX(self):
+        return self.__marginX
+
+    def getMarginY(self):
+        return self.__marginY
+
+    def getLayout(self):
+        return self.__layout
+
     def isFadeAnimationRunning(self):
         return self.__fadeAnimation
+
+    def isScaleAnimationRunning(self):
+        return self.__scaleAnimation

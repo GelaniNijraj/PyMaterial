5fbe44cfe3d539a84f4da7771d0ee8a97b013555
Nijraj Gelani <gelaninijraj@gmail.com>
2016-06-22 17:08:12 +0530

    Created MValueAnimator class for manipulating raw animation values. Created MAnimationThread class intended to be used as a single thread to run all the animations on.

---
+++ demo/fluent_animations_demo.py
@@ -0,0 +1,36 @@
+__author__ = "MaitreyaBuddha"
+
+import sys
+from PySide.QtGui import *
+from MUtilities import MColors
+from MComponents.MTestComponent import MTestComponent
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setWindowTitle("Reveal test")
+        self.setGeometry(100, 100, 500, 500)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), MColors.BACKGROUND_DARK)
+        self.setPalette(p)
+
+    def addComponents(self):
+        layout = QGridLayout()
+        self.test1 = MTestComponent()
+        cancel = QPushButton("Cancel Animation")
+        cancel.clicked.connect(self.cancel_animation)
+        layout.addWidget(self.test1)
+        layout.addWidget(cancel)
+        self.setLayout(layout)
+
+    def cancel_animation(self):
+        self.test1.trigger_cancel()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    win.addComponents()
+    app.exec_()
+    sys.exit()
